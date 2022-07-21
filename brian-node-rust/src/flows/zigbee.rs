482f8cc0bb0a5c88b41568ef7c71cb29646339b6
Brian May <brian@linuxpenguins.xyz>
2022-07-21 15:37:25 +1000

    Don't thank you for closing door unless we complained

--- brian-node-rust/src/flows/zigbee.rs
+++ brian-node-rust/src/flows/zigbee.rs
@@ -31,9 +31,9 @@ pub fn start(subscriptions: &mut Subscriptions, message_sink: &TxPipe<String>) {
         .subscribe_to_string("zigbee2mqtt/Dining/door")
         .filter_map(third_reality_door_sensor)
         .map(|door_sensor| !door_sensor.contact)
-        .diff()
-        .changed()
         .delay_true(Duration::from_secs(30))
+        .diff_with_initial_value(Some(false))
+        .changed()
         .timer_true(Duration::from_secs(60))
         .map(|state| match state {
             true => "Please close the front door".to_string(),
