c0368a47f65691ed1251e94ebd20f06271877040
Brian May <brian@linuxpenguins.xyz>
2022-07-27 13:11:16 +1000

    Delete tesla stuff
    Moved to home-assistant.

--- brian-node-rust/src/flows/mod.rs
+++ brian-node-rust/src/flows/mod.rs
@@ -2,5 +2,4 @@ pub mod common;
 pub mod espresence;
 pub mod google;
 pub mod robotica;
-pub mod tesla;
 pub mod zigbee;
--- brian-node-rust/src/main.rs
+++ brian-node-rust/src/main.rs
@@ -4,7 +4,6 @@ mod http;
 use anyhow::Result;
 use flows::common::message_sink;
 use flows::google;
-use flows::tesla;
 use flows::zigbee;
 use robotica_node_rust::sources::mqtt::MqttOut;
 
@@ -29,7 +28,6 @@ fn setup_pipes(mqtt: &MqttOut) -> Subscriptions {
     let mut subscriptions: Subscriptions = Subscriptions::new();
     let message_sink = message_sink(&mut subscriptions, mqtt);
 
-    tesla::start(&mut subscriptions, &message_sink);
     zigbee::start(&mut subscriptions, &message_sink, mqtt);
     google::start(&mut subscriptions, mqtt);
 
