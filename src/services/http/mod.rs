bc0e393ea4754da26feb32696e33a6d5782d7914
Brian May <brian@linuxpenguins.xyz>
2022-10-21 20:07:27 +1100

    More work on backend

--- src/services/http/mod.rs
+++ src/services/http/mod.rs
@@ -2,7 +2,6 @@
 mod oidc;
 mod urls;
 
-use std::include_str;
 use std::path::PathBuf;
 use std::str::Utf8Error;
 use std::sync::Arc;
@@ -11,15 +10,15 @@ use std::{collections::HashMap, env};
 use axum::body::{boxed, Body};
 use axum::extract::ws::{Message, WebSocket};
 use axum::extract::WebSocketUpgrade;
-use axum::http::Uri;
-use axum::response::{Html, IntoResponse, Response};
+use axum::http::Request;
+use axum::response::{IntoResponse, Response};
 use axum::{extract::Query, routing::get, Extension, Router};
 use axum_sessions::extractors::ReadableSession;
 use axum_sessions::{async_session::MemoryStore, extractors::WritableSession};
 use axum_sessions::{SameSite, SessionLayer};
 use base64::decode;
 use futures::{sink::SinkExt, stream::StreamExt};
-use maud::{html, Markup};
+use maud::{html, Markup, DOCTYPE};
 use reqwest::StatusCode;
 use serde::{Deserialize, Serialize};
 use thiserror::Error;
@@ -119,43 +118,10 @@ async fn server(
     let oidc = Arc::new(oidc);
 
     let app = Router::new()
-        // .route("/", get(root))
-        .route("/test", get(test))
-        .route("/login", get(login))
+        .route("/", get(root))
         .route("/openid_connect_redirect_uri", get(oidc_callback))
         .route("/websocket", get(websocket_handler))
-        .route("/api/hello", get(root))
-        .fallback(get(|req| async move {
-            match ServeDir::new("./dist").oneshot(req).await {
-                Ok(res) => {
-                    let status = res.status();
-                    match status {
-                        StatusCode::NOT_FOUND => {
-                            let index_path = PathBuf::from("./dist").join("index.html");
-                            let index_content = match fs::read_to_string(index_path).await {
-                                Err(_) => {
-                                    return Response::builder()
-                                        .status(StatusCode::NOT_FOUND)
-                                        .body(boxed(Body::from("index file not found")))
-                                        .unwrap()
-                                }
-                                Ok(index_content) => index_content,
-                            };
-
-                            Response::builder()
-                                .status(StatusCode::OK)
-                                .body(boxed(Body::from(index_content)))
-                                .unwrap()
-                        }
-                        _ => res.map(boxed),
-                    }
-                }
-                Err(err) => Response::builder()
-                    .status(StatusCode::INTERNAL_SERVER_ERROR)
-                    .body(boxed(Body::from(format!("error: {err}"))))
-                    .expect("error response"),
-            }
-        }))
+        .fallback(get(fallback_handler))
         .layer(Extension(http_state))
         .layer(Extension(oidc))
         .layer(session_layer)
@@ -171,116 +137,154 @@ async fn server(
     Ok(())
 }
 
+fn get_user(session: &ReadableSession) -> Option<String> {
+    session.get::<String>("user")
+}
+
+async fn fallback_handler(
+    session: ReadableSession,
+    oidc_client: Extension<Arc<Client>>,
+    req: Request<Body>,
+) -> Response {
+    if get_user(&session).is_none() {
+        let origin_url = req.uri().path_and_query().unwrap().as_str();
+        let auth_url = oidc_client.get_auth_url(origin_url);
+        return Response::builder()
+            .status(StatusCode::FOUND)
+            .header("Location", auth_url)
+            .body(Body::empty())
+            .unwrap()
+            .into_response();
+    }
+
+    match ServeDir::new("./dist").oneshot(req).await {
+        Ok(response) => {
+            let status = response.status();
+            match status {
+                StatusCode::NOT_FOUND => {
+                    let index_path = PathBuf::from("./dist").join("index.html");
+                    let index_content = match fs::read_to_string(index_path).await {
+                        Err(_) => {
+                            return Response::builder()
+                                .status(StatusCode::NOT_FOUND)
+                                .body(boxed(Body::from("index file not found")))
+                                .unwrap()
+                        }
+                        Ok(index_content) => index_content,
+                    };
+
+                    Response::builder()
+                        .status(StatusCode::OK)
+                        .body(boxed(Body::from(index_content)))
+                        .unwrap()
+                }
+                _ => response.map(boxed),
+            }
+        }
+        Err(err) => Response::builder()
+            .status(StatusCode::INTERNAL_SERVER_ERROR)
+            .body(boxed(Body::from(format!("error: {err}"))))
+            .expect("error response"),
+    }
+}
+
+fn nav_bar() -> Markup {
+    html! {
+        nav class="navbar navbar-expand-sm navbar-dark bg-dark navbar-fixed-top" role="navigation" {
+            div class="container-fluid" {
+                a class="navbar-brand" href="/" { "Robotica" }
+                button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation" {
+                    span class="navbar-toggler-icon" {}
+                }
+                div class="collapse navbar-collapse" id="navbarNav" {
+                    div class="navbar-nav" {
+                        li class="nav-item" {
+                            a class="nav-link" href="/chat" { "Chat" }
+                        }
+                        li class="nav-item" {
+                            a class="nav-link" aria-current="page" href="/login" { "Login" }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
 #[allow(clippy::unused_async)]
-async fn root(http_config: Extension<Arc<HttpConfig>>, session: ReadableSession) -> Markup {
+async fn root(session: ReadableSession) -> Markup {
     let build_date = env::var("BUILD_DATE").unwrap_or_else(|_| "unknown".to_string());
     let vcs_ref = env::var("VCS_REF").unwrap_or_else(|_| "unknown".to_string());
 
-    let user = session.get::<String>("user");
-
-    let login_url = http_config.generate_url_or_default("/login");
+    let user = get_user(&session);
 
     html!(
+        (DOCTYPE)
         html {
             head {
                 title { "Robotica" }
+                link rel="stylesheet" href="bootstrap.min.css" {}
+                script src="bootstrap.min.js" {}
             }
             body {
+                ( nav_bar() )
                 h1 { "Robotica" }
-                p { "You are boring. Go away." }
-                p { (format!("{:?}", user)) }
-                p { "Build date: " (build_date) }
-                p { "VCS ref: " (vcs_ref) }
-                a href=(login_url) { "Login" }
-            }
-        }
-    )
-}
-
-// Include utf-8 file at **compile** time.
-#[allow(clippy::unused_async)]
-async fn test() -> Html<&'static str> {
-    Html(include_str!("chat.html"))
-}
-
-#[allow(clippy::unused_async)]
-async fn login(
-    http_config: Extension<Arc<HttpConfig>>,
-    oidc_client: Extension<Arc<Client>>,
-    origin_url: Uri,
-) -> Markup {
-    let origin_url = origin_url.path_and_query().unwrap().as_str();
-    let auth_url = oidc_client.get_auth_url(origin_url);
-    let root_url = http_config.generate_url_or_default("/");
-
-    html!(
-        html {
-            head {
-                title { "Robotica - Login" }
-            }
-            body {
-                h1 { "Login" }
-                p { "Do I know you?" }
-                p { "Links"
-                    ul {
-                        li { a href=(root_url) { "Home" } }
-                        li { a href=(auth_url) { "Login" } }
-                    }
-                }
+                p { @match user {
+                    Some(user) => ( format!("Hello, {}!", user) ),
+                    None => ( "You are not logged in!" ),
+                } }
+                p { "Build date: " (build_date) ", VCS ref: " (vcs_ref) }
             }
         }
     )
 }
 
 async fn oidc_callback(
-    http_path: Extension<Arc<HttpConfig>>,
+    http_config: Extension<Arc<HttpConfig>>,
     oidc_client: Extension<Arc<Client>>,
     Query(params): Query<HashMap<String, String>>,
     mut session: WritableSession,
-) -> Markup {
-    let root_url = http_path.generate_url_or_default("/");
-
+) -> Response {
     let code = params
         .get("code")
         .cloned()
         .unwrap_or_else(|| "".to_string());
 
+    let state = params
+        .get("state")
+        .cloned()
+        .unwrap_or_else(|| "/".to_string());
+
     let result = oidc_client.request_token(&code).await;
 
-    let user = match result {
+    match result {
         Ok((_token, user_info)) => {
             session.insert("user", &user_info.name).unwrap();
-            Ok(format!("{:?}", user_info))
+
+            let url = http_config.generate_url_or_default(&state);
+
+            Response::builder()
+                .status(StatusCode::FOUND)
+                .header("Location", url)
+                .body(Body::empty())
+                .unwrap()
+                .into_response()
         }
         Err(e) => {
             session.destroy();
-            Err(format!("{}", e))
-        }
-    };
-
-    html!(
-        html {
-            head {
-                title { "Robotica - Login" }
-            }
-            body {
-                h1 { "Login" }
-                @match user {
-                    Ok(user) => {
-                        p { "YES! I know you!" }
-                        p { (user) }
+            html!(
+                    html {
+                    head {
+                        title { "Robotica - Login" }
                     }
-                    Err(err) => {
-                        p { "NO! I don't know you!" }
-                        p { (err) }
+                    body {
+                        h1 { ( format!("Login Failed: {e}") ) }
                     }
                 }
-                p {
-                    a href=(root_url) { "Home" }
-                }
-            }
+            )
+            .into_response()
         }
-    )
+    }
 }
 
 #[allow(clippy::unused_async)]
@@ -290,7 +294,7 @@ async fn websocket_handler(
     session: ReadableSession,
 ) -> Response {
     #[allow(clippy::option_if_let_else)]
-    if let Some(_name) = session.get::<String>("user") {
+    if let Some(_name) = get_user(&session) {
         info!("Accessing websocket");
         ws.on_upgrade(|socket| websocket(socket, state))
             .into_response()
