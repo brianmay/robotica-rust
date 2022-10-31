437115972e55e4316151b66b908b99d6f9a3e02a
Brian May <brian@linuxpenguins.xyz>
2022-10-31 16:59:53 +1100

    Updates to room views

--- brian-frontend/src/components/rooms.rs
+++ brian-frontend/src/components/rooms.rs
@@ -1,12 +1,36 @@
+use std::ops::Deref;
+
+use robotica_common::websocket::MqttMessage;
+use robotica_frontend::services::websocket::event_bus::{Command, EventBus};
 use yew::prelude::*;
 
 use robotica_frontend::components::button::{Button, LightProps, MusicProps, SwitchProps};
 use robotica_frontend::services::controllers::{Action, Icon};
+use yew_agent::Bridged;
 
 use super::require_connection::RequireConnection;
 
 #[function_component(BrianRoom)]
 pub fn brian_room() -> Html {
+    let callback = { Callback::from(move |_| {}) };
+
+    let events = use_mut_ref(|| EventBus::bridge(callback));
+
+    let light_power = use_state(|| None);
+
+    let callback = {
+        let light_power = light_power.clone();
+        Callback::from(move |msg: MqttMessage| {
+            light_power.set(Some(msg.payload));
+        })
+    };
+
+    use_ref(|| {
+        let topic = "state/Brian/Light/power".to_string();
+        let subscribe = Command::Subscribe { topic, callback };
+        events.borrow_mut().send(subscribe);
+    });
+
     let light_icon = Icon::new("light");
     let wake_up_icon = Icon::new("wake_up");
     let fan_icon = Icon::new("fan");
@@ -15,10 +39,15 @@ pub fn brian_room() -> Html {
         <RequireConnection>
             <h1>{ "Brian's Room" }</h1>
 
-            <h2>{"Lights"}</h2>
+            <h2>
+                {"Lights"}
+                if let Some(power) = light_power.deref() {
+                    {" - "} {power}
+                }
+            </h2>
             <div class="buttons">
-                <Button<LightProps> name={"Brian Auto"} topic_substr={"Brian/Light"} action={Action::Toggle} icon={light_icon.clone()} scene={"auto"} priority={100} />
-                <Button<LightProps> name={"Brian On"} topic_substr={"Brian/Light"} action={Action::Toggle} icon={light_icon.clone()} scene={"default"} priority={100} />
+                <Button<LightProps> name={"Auto"} topic_substr={"Brian/Light"} action={Action::Toggle} icon={light_icon.clone()} scene={"auto"} priority={100} />
+                <Button<LightProps> name={"On"} topic_substr={"Brian/Light"} action={Action::Toggle} icon={light_icon.clone()} scene={"default"} priority={100} />
             </div>
 
             <h2>{"Music"}</h2>
@@ -30,13 +59,13 @@ pub fn brian_room() -> Html {
 
             <h2>{"Switches"}</h2>
             <div class="buttons">
-                <Button<SwitchProps> name={"Brian Fan"} topic_substr={"Brian/Fan"} action={Action::Toggle} icon={fan_icon} />
+                <Button<SwitchProps> name={"Fan"} topic_substr={"Brian/Fan"} action={Action::Toggle} icon={fan_icon} />
             </div>
 
             <h2>{"Passage"}</h2>
             <div class="buttons">
-                <Button<LightProps> name={"Passage Auto"} topic_substr={"Passage/Light"} action={Action::Toggle} icon={light_icon.clone()} scene={"auto"} priority={100} />
-                <Button<LightProps> name={"Passage On"} topic_substr={"Passage/Light"} action={Action::Toggle} icon={light_icon} scene={"default"} priority={100} />
+                <Button<LightProps> name={"Auto"} topic_substr={"Passage/Light"} action={Action::Toggle} icon={light_icon.clone()} scene={"auto"} priority={100} />
+                <Button<LightProps> name={"On"} topic_substr={"Passage/Light"} action={Action::Toggle} icon={light_icon} scene={"default"} priority={100} />
             </div>
         </RequireConnection>
     )
@@ -58,8 +87,8 @@ pub fn dining_room() -> Html {
 
             <h2>{"Passage"}</h2>
             <div class="buttons">
-                <Button<LightProps> name={"Passage Auto"} topic_substr={"Passage/Light"} action={Action::Toggle} icon={light_icon.clone()} scene={"auto"} priority={100} />
-                <Button<LightProps> name={"Passage On"} topic_substr={"Passage/Light"} action={Action::Toggle} icon={light_icon} scene={"default"} priority={100} />
+                <Button<LightProps> name={"Auto"} topic_substr={"Passage/Light"} action={Action::Toggle} icon={light_icon.clone()} scene={"auto"} priority={100} />
+                <Button<LightProps> name={"On"} topic_substr={"Passage/Light"} action={Action::Toggle} icon={light_icon} scene={"default"} priority={100} />
             </div>
         </RequireConnection>
     )
