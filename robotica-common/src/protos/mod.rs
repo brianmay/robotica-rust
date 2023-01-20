d65e531f784988a53afc31aa061ed729c5a4c5e4
Brian May <brian@linuxpenguins.xyz>
2023-01-21 10:51:14 +1100

    Silence clippy alerts in auto generated code

--- robotica-common/src/protos/mod.rs
+++ robotica-common/src/protos/mod.rs
@@ -1,4 +1,7 @@
 //! This module contains the generated protobuf code for the robotica protocol.
 #![allow(missing_docs)]
+#![allow(clippy::doc_markdown)]
+#![allow(clippy::missing_const_for_fn)]
+#![allow(clippy::trivially_copy_pass_by_ref)]
 
 include!(concat!(env!("OUT_DIR"), "/protos.websocket.rs"));
