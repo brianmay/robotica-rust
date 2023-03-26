90ec78d36fff1afc640f16842dc0ac4ce2a358a6
Brian May <brian@linuxpenguins.xyz>
2023-03-26 15:31:32 +1100

    Add nix build support for robotica-slint

--- flake.nix
+++ flake.nix
@@ -2,34 +2,58 @@
   description = "A very basic flake";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs }:
-    let
-      # Generate a user-friendly version number.
-      version = builtins.substring 0 8 self.lastModifiedDate;
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachSystem flake-utils.lib.allSystems (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        pkg = pkgs.rustPlatform.buildRustPackage {
+          pname = "robotica-slint";
+          version = "0.0.1";
+          src = ./.;
+          cargoBuildFlags = "-p robotica-slint";
 
-      # System types to support.
-      supportedSystems = [ "x86_64-linux" ];
-
-      # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-
-      # Nixpkgs instantiated for supported system types.
-      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
-    in {
-      devShells = forAllSystems (system:
-        let pkgs = nixpkgsFor.${system};
-        in {
-          default = pkgs.mkShell {
-            buildInputs = with pkgs; [
-              cargo
-              rustc
-              clippy
-              pkgconfig
-              openssl
-              protobuf
-            ];
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+            outputHashes = {
+              "getrandom-0.2.8" =
+                "sha256-B6xHsgEgJMYG2xj2pNRnjD/b71fSTRVC9Jv/fH3b2Ok=";
+              "rumqttc-0.18.0" =
+                "sha256-bZA887J8+G8JReMYT2elBtq7iRSM/mNy7/9wlPCNxrI=";
+            };
           };
-        });
-    };
+
+          nativeBuildInputs = with pkgs; [
+            pkgconfig
+            openssl
+            protobuf
+            fontconfig
+            freetype
+            xorg.libxcb
+          ];
+
+          PKG_CONFIG_PATH =
+            "${pkgs.openssl.dev}/lib/pkgconfig:${pkgs.fontconfig.dev}/lib/pkgconfig:${pkgs.freetype.dev}/lib/pkgconfig";
+        };
+
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            #rustup
+            #cargo-cross
+            cargo
+            rustc
+            clippy
+            pkgconfig
+            openssl
+            protobuf
+
+            # slint
+            fontconfig
+            xorg.libxcb
+          ];
+        };
+        packages = { robotica-slint = pkg; };
+      });
 }
