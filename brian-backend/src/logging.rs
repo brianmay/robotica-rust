0b8bf5cb23edcb35adfdaeb56a9a0b82d5367273
Brian May <brian@linuxpenguins.xyz>
2024-06-30 17:38:32 +1000

    Fix logging to openobserve instance

--- brian-backend/src/logging.rs
+++ brian-backend/src/logging.rs
@@ -1,15 +1,8 @@
 use data_encoding::BASE64;
-use opentelemetry::{global, KeyValue};
-use opentelemetry_otlp::WithExportConfig;
-use opentelemetry_sdk::{
-    metrics::{
-        reader::{DefaultAggregationSelector, DefaultTemporalitySelector},
-        MeterProviderBuilder, PeriodicReader, SdkMeterProvider,
-    },
-    runtime,
-    trace::{BatchConfig, RandomIdGenerator, Sampler, Tracer},
-    Resource,
-};
+use opentelemetry::KeyValue;
+use opentelemetry_appender_tracing::layer::OpenTelemetryTracingBridge;
+use opentelemetry_otlp::{ExportConfig, WithExportConfig};
+use opentelemetry_sdk::{metrics::SdkMeterProvider, runtime, trace::Tracer, Resource};
 use opentelemetry_semantic_conventions::{
     resource::{DEPLOYMENT_ENVIRONMENT, SERVICE_NAME, SERVICE_VERSION},
     SCHEMA_URL,
@@ -19,9 +12,8 @@ use serde::Deserialize;
 use tap::Pipe;
 use thiserror::Error;
 use tonic::metadata::{errors::InvalidMetadataValue, MetadataMap};
-use tracing_core::Level;
 use tracing_opentelemetry::{MetricsLayer, OpenTelemetryLayer};
-use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
 
 #[derive(Deserialize)]
 pub struct RemoteConfig {
@@ -78,67 +70,61 @@ pub enum Error {
 
     #[error("Metrics error: {0}")]
     Metrics(#[from] opentelemetry::metrics::MetricsError),
-}
 
-// Construct MeterProvider for MetricsLayer
-fn init_meter_provider(config: &Config) -> Result<SdkMeterProvider, Error> {
-    let reader = if let Some(remote) = &config.remote {
-        let exporter = opentelemetry_otlp::new_exporter()
-            .tonic()
-            .with_endpoint(remote.endpoint.clone())
-            .with_metadata(otlp_metadata(remote)?)
-            .build_metrics_exporter(
-                Box::new(DefaultAggregationSelector::new()),
-                Box::new(DefaultTemporalitySelector::new()),
-            )?;
-
-        PeriodicReader::builder(exporter, runtime::Tokio)
-            .with_interval(std::time::Duration::from_secs(30))
-            .build()
-            .pipe(Some)
-    } else {
-        None
-    };
-
-    // For debugging in development
-    let stdout_reader = PeriodicReader::builder(
-        opentelemetry_stdout::MetricsExporter::default(),
-        runtime::Tokio,
-    )
-    .build();
-
-    let meter_provider = MeterProviderBuilder::default()
-        .with_resource(resource(config))
-        .pipe(|p| {
-            if let Some(reader) = reader {
-                p.with_reader(reader)
-            } else {
-                p
-            }
-        })
-        .with_reader(stdout_reader)
-        .build();
-
-    global::set_meter_provider(meter_provider.clone());
-
-    Ok(meter_provider)
+    #[error("Log error: {0}")]
+    Log(#[from] opentelemetry::logs::LogError),
 }
 
 // Construct Tracer for OpenTelemetryLayer
-fn init_tracer(config: &Config, remote: &RemoteConfig) -> Result<Tracer, Error> {
+fn init_tracer(resource: &Resource, remote: &RemoteConfig) -> Result<Tracer, Error> {
     opentelemetry_otlp::new_pipeline()
         .tracing()
-        .with_trace_config(
-            opentelemetry_sdk::trace::Config::default()
-                // Customize sampling strategy
-                .with_sampler(Sampler::ParentBased(Box::new(Sampler::TraceIdRatioBased(
-                    1.0,
-                ))))
-                // If export trace to AWS X-Ray, you can use XrayIdGenerator
-                .with_id_generator(RandomIdGenerator::default())
-                .with_resource(resource(config)),
+        .with_exporter(
+            opentelemetry_otlp::new_exporter()
+                .tonic()
+                .with_endpoint(remote.endpoint.clone())
+                .with_metadata(otlp_metadata(remote)?),
         )
-        .with_batch_config(BatchConfig::default())
+        .with_trace_config(
+            opentelemetry_sdk::trace::Config::default().with_resource(resource.clone()),
+        )
+        .install_batch(runtime::Tokio)?
+        .pipe(Ok)
+}
+
+fn init_metrics(
+    resource: &Resource,
+    remote: &RemoteConfig,
+) -> Result<opentelemetry_sdk::metrics::SdkMeterProvider, Error> {
+    let export_config = ExportConfig {
+        endpoint: remote.endpoint.clone(),
+        ..ExportConfig::default()
+    };
+    let provider = opentelemetry_otlp::new_pipeline()
+        .metrics(runtime::Tokio)
+        .with_exporter(
+            opentelemetry_otlp::new_exporter()
+                .tonic()
+                .with_export_config(export_config)
+                .with_metadata(otlp_metadata(remote)?),
+        )
+        .with_resource(resource.clone())
+        .build();
+    match provider {
+        Ok(provider) => Ok(provider),
+        Err(err) => Err(err.into()),
+    }
+}
+
+fn init_logs(
+    resource: &Resource,
+    remote: &RemoteConfig,
+) -> Result<opentelemetry_sdk::logs::LoggerProvider, Error> {
+    opentelemetry_otlp::new_pipeline()
+        .logging()
+        .with_log_config(opentelemetry_sdk::logs::Config::default().with_resource(resource.clone()))
+        // Use after next release of opentelemetry-otlp
+        // .with_resource(resource.clone())
         .with_exporter(
             opentelemetry_otlp::new_exporter()
                 .tonic()
@@ -151,35 +137,69 @@ fn init_tracer(config: &Config, remote: &RemoteConfig) -> Result<Tracer, Error> 
 
 // Initialize tracing-subscriber and return OtelGuard for opentelemetry-related termination processing
 pub fn init_tracing_subscriber(config: &Config) -> Result<OtelGuard, Error> {
-    let meter_provider = init_meter_provider(config)?;
+    let resource = resource(config);
+
+    // Add a tracing filter to filter events from crates used by opentelemetry-otlp.
+    // The filter levels are set as follows:
+    // - Allow `info` level and above by default.
+    // - Restrict `hyper`, `tonic`, and `reqwest` to `error` level logs only.
+    // This ensures events generated from these crates within the OTLP Exporter are not looped back,
+    // thus preventing infinite event generation.
+    // Note: This will also drop events from these crates used outside the OTLP Exporter.
+    // For more details, see: https://github.com/open-telemetry/opentelemetry-rust/issues/761
+
+    // FIXME - don't use unwrap!
+    #[allow(clippy::unwrap_used)]
+    let filter = EnvFilter::new("info")
+        .add_directive("hyper=error".parse().unwrap())
+        .add_directive("tonic=error".parse().unwrap())
+        .add_directive("reqwest=error".parse().unwrap());
 
     let layer = tracing_subscriber::registry()
-        .with(tracing_subscriber::filter::LevelFilter::from_level(
-            Level::INFO,
-        ))
-        .with(tracing_subscriber::fmt::layer())
-        .with(MetricsLayer::new(meter_provider.clone()));
+        .with(filter)
+        .with(tracing_subscriber::fmt::layer());
 
     if let Some(remote) = &config.remote {
+        let meter_provider = init_metrics(&resource, remote)?;
+        let logger_provider = init_logs(&resource, remote)?;
+
         layer
-            .with(OpenTelemetryLayer::new(init_tracer(config, remote)?))
+            .with(MetricsLayer::new(meter_provider.clone()))
+            .with(OpenTelemetryLayer::new(init_tracer(&resource, remote)?))
+            .with(OpenTelemetryTracingBridge::new(&logger_provider))
             .init();
+        Ok(OtelGuard {
+            meter_provider: Some(meter_provider),
+            logger_provider: Some(logger_provider),
+        })
     } else {
         layer.init();
-    }
 
-    Ok(OtelGuard { meter_provider })
+        Ok(OtelGuard {
+            meter_provider: None,
+            logger_provider: None,
+        })
+    }
 }
 
 pub struct OtelGuard {
-    meter_provider: SdkMeterProvider,
+    meter_provider: Option<SdkMeterProvider>,
+    logger_provider: Option<opentelemetry_sdk::logs::LoggerProvider>,
 }
 
 impl Drop for OtelGuard {
     fn drop(&mut self) {
-        if let Err(err) = self.meter_provider.shutdown() {
-            eprintln!("{err:?}");
+        if let Some(provider) = self.meter_provider.take() {
+            if let Err(err) = provider.shutdown() {
+                eprintln!("{err:?}");
+            }
         }
+        if let Some(provider) = self.logger_provider.take() {
+            if let Err(err) = provider.shutdown() {
+                eprintln!("{err:?}");
+            }
+        }
+
         opentelemetry::global::shutdown_tracer_provider();
     }
 }
