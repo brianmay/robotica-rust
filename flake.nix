f396f9e09f9dbe3149a3b4f77b2bbd92290b9e14
Brian May <brian@linuxpenguins.xyz>
2026-05-24 16:31:36 +1000

    chore: update wasm-bindgen-cli in nixos

--- flake.nix
+++ flake.nix
@@ -50,8 +50,8 @@
           wasm-bindgen-cli = pkgs.buildWasmBindgenCli rec {
             src = pkgs.fetchCrate {
               pname = "wasm-bindgen-cli";
-              version = "0.2.121";
-              hash = "sha256-ZOMgFNOcGkO66Jz/Z83eoIu+DIzo3Z/vq6Z5g6BDY/w=";
+              version = "0.2.122";
+              hash = "sha256-vO4RSxi/sMWxmsEs3GuljdMfIRSu75A+Q+c5wgYToRU=";
             };
             # src = pkgs.fetchFromGitHub {
             #   owner = "rustwasm";
@@ -63,7 +63,7 @@
             cargoDeps = pkgs.rustPlatform.fetchCargoVendor {
               inherit src;
               inherit (src) pname version;
-              hash = "sha256-DPdCDPTAPBrbqLUqnCwQu1dePs9lGg85JCJOCIr9qjU=";
+              hash = "sha256-Inup6vvJSG5ghNyeDPyZbfZo4d0LsMG2OJfStoaeDBs=";
               # hash = pkgs.lib.fakeHash;
             };
           };
